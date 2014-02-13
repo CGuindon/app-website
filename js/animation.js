42db54cfc0cc2521dff69f83bc9c19e6cd9cf392
CGuindon <Caro@Caroline-Andree-Guindons-MacBook-Pro.local>
2014-02-13 09:07:09 -0500

    Added Ajax

--- js/animation.js
+++ js/animation.js
@@ -2,7 +2,7 @@
 var $features = $('.features img');
 
 
-// Ships Section
+// Features Section
 $('.features-section').waypoint(function () {
 	$features.eq(0).addClass('js-features-delay-1');
 	$features.eq(2).addClass('js-features-delay-3');
